8926e9bc102e9b1085d9f05ff88770f56ab7e7fc
Leah White <leahawhite@gmail.com>
2019-07-14 22:37:38 -0700

    added cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require ('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
@@ -12,7 +13,9 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}))
 app.use(helmet())
 
 app.get('/', (req, res) => {
