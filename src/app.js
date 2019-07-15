2e8e0ceb72f82122742eb04ec0bcd86f711d1781
Leah White <leahawhite@gmail.com>
2019-07-15 13:04:44 -0700

    places working

--- src/app.js
+++ src/app.js
@@ -13,16 +13,23 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}))
+app.use(cors())
 app.use(helmet())
 
-const data = require('../data.js')
+const places = require('./places.js');
 
 app.get('/', (req, res) => {
-  // res.send('Hello, world!')
-  res.json(data)
+  res.send('Hello, world!')
+})
+
+app.get('/places', (req, res) => {
+  const { searchTerm = '', category='all', neighborhood='All Portland', sort='' } = req.query
+  let results = places.filter(place => (place.name.toLowerCase().includes(searchTerm.toLowerCase())
+      || place.descriptors.includes(searchTerm.toLowerCase()) || place.category.includes(searchTerm.toLowerCase())) 
+      && (place.category.includes(category) || category === 'all') 
+      && (neighborhood === place.neighborhood || neighborhood === 'All Portland'))
+  
+  res.json(results)
 })
 
 app.use(function errorHandler(error, req, res, next) {
