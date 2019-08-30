fe0017d378ad3532aaf45c72ad4be27681d52a2b
Leah White <leahawhite@gmail.com>
2019-08-29 21:32:31 -0700

    cleanup

--- seeds/arrayaggcategory.sql
+++
@@ -1,7 +0,0 @@
-select places.name,
-array_agg(category.category_name) as category
-from category inner join place_category
-on category.id=place_category.category_id
-inner join places on places.id=place_category.place_id
-group by places.id
-order by places.id
--- seeds/seed.xss_reviews.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO reviews (id, rating, text, place_id, user_id)
-VALUES
-  (911, 5, 'This text contains an intentionally broken image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie); alert(''you just got pretend hacked! oh noes!'');">. The image will try to load, when it fails, <strong>it executes malicious JavaScript</strong>', 1, 1);
