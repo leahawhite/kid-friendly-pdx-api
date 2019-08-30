55f9b8d4e0d680bcae54313cb9bbfdf1139ce9fe
Leah White <leahawhite@gmail.com>
2019-08-30 11:36:46 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres:quesadilla@localhost/kid_friendly_pdx',
   TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres:quesadilla@localhost/kid_friendly_pdx_test',
-  JWT_SECRET: process.env.JWT_SECRET || 'buffaloroam',
+  JWT_SECRET: process.env.JWT_SECRET,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
 }
